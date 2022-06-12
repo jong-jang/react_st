28f16852a1d870509bb26ec1ecd2aec21a47c1a5
jong-jang <jjh0299@gmail.com>
2022-06-12 12:29:17 +0900

    04 3단계를 파일로 나눠서

--- day04/src/components/Test4.js
+++ day04/src/components/Test4.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Test4Ani from './Test4Ani';
 import Test4Dis from './Test4Dis';
-import Test4Name from './Test4Name';
+import Test4Name from './Test4Name'
 
 const Test4 = () => {
     const [ani, setAni] = useState('강아지')
--- day04/src/components/Test4Ani.js
+++ day04/src/components/Test4Ani.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 const Test4Ani = ({ani, onAni}) => {
     return (
