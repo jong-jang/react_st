c864c00db57b72305e4e91067d42f77a80d5905a
jong-jang <jjh0299@gmail.com>
2022-06-12 12:13:02 +0900

    :seeding: 3일차 리액트

---
+++ day04/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Test4 from './components/Test4'
+const App = () => {
+  return (
+    <div>
+      <Test4 />
+    </div>
+  );
+};
+
+export default App;
---
+++ day04/src/components/Test4.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Test4Ani from './Test4Ani';
+import Test4Dis from './Test4Dis';
+import Test4Name from './Test4Name';
+
+const Test4 = () => {
+    const [ani, setAni] = useState('강아지')
+    const [text, setText] = useState('아무개')
+
+    const onText = (e) => {
+        const {value} = e.target
+        setText(value)
+    }
+    const onAni = (e) => {
+        const {value} = e.target
+        setAni(value)
+    }
+    
+    return (
+        <div>
+            <Test4Name text={text} onText={onText}/>
+            <hr />
+            <Test4Ani ani={ani} onAni={onAni}/>
+            <hr />
+            <Test4Dis text={text} ani={ani}/>
+        </div>
+    );
+};
+
+export default Test4;
