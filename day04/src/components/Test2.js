3ff7b71f3b3dbf7d31f1cf95407e06ab7d1623cd
jong-jang <jjh0299@gmail.com>
2022-06-12 12:10:42 +0900

    02 팝업창 연습

---
+++ day04/src/components/Test2.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Test2Modal from './Test2Modal';
+import '../assets/css/reset.css'
+import './Test2.css'
+
+const Test2 = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const onOpen = () => {
+        setIsOpen(true)
+    }
+    const onClose = () => {
+        setIsOpen(false)
+    }
+    
+    return (
+        <div>
+            <button onClick={onOpen}>팝업띄우기</button>
+            {
+                isOpen && <Test2Modal onClose={onClose}/>
+            }
+        </div>
+    );
+};
+
+export default Test2;
