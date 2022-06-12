c73771141c50058b7f87f6885f0451a6817c8b4c
jong-jang <jjh0299@gmail.com>
2022-06-12 17:47:02 +0900

    :seedling: 4일차 리액트

--- day04/src/App.js
+++ day04/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import Test4 from './components/Test4'
+import Test7 from './components/Test7'
+
+
+
 const App = () => {
+  
   return (
     <div>
-      <Test4 />
+      <Test7 />
     </div>
   );
 };
