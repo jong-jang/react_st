00a0389f7c17f0b5ae6552d5771d3a3e8bfc6a6f
jong-jang <jjh0299@gmail.com>
2022-07-10 17:50:22 +0900

    다음주꺼 미리 구조만

---
+++ pic-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import GlobalStyle from './components/styled/Global';
+
+const App = () => {
+  return (
+    <>
+    <GlobalStyle />
+    
+      <h1>제목1</h1>
+      <h1>제목1</h1>
+      <h1>제목1</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ pic-app/src/components/gallery/GalleyList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GalleyList = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default GalleyList;
